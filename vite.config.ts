3072af7b0f5c525fbfc91dacb181109d55c6b5ab
ShirleyAar <saragon@espol.edu.ec>
2025-12-14 18:27:44 -0500

    fix: Editar perfil y boton logout

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/finmate/',
+  base: '/klimba/',
   server: {
     host: "::",
     port: 8080,
